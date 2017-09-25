d835446b199cdc346549be7e972bde0aaf38d077
Jørgen Hanssen <jorkiz97@gmail.com>
2017-09-25 20:36:16 +0200

    Moved all tests out of subdirectories

--- main.go
+++ main.go
@@ -5,14 +5,13 @@ import (
 	"net/http"
 
 	"github.com/Hanssen97/cloud_assignment1/constants"
-	"github.com/Hanssen97/cloud_assignment1/handlers"
 
 	"google.golang.org/appengine"
 )
 
 func main() {
-	http.HandleFunc("/", handlers.HomePage)
-	http.HandleFunc("/projectinfo/v1/github.com/", handlers.Repo)
+	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/projectinfo/v1/github.com/", Repo)
 
 	fmt.Println("App running on port " + constants.PORT)
 	http.ListenAndServe(":"+constants.PORT, nil)
