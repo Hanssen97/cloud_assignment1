72423d13c869b68aaca4e1a9f27f7d16933e8bd2
Jørgen Hanssen <jorkiz97@gmail.com>
2017-09-26 14:37:35 +0200

    Re-applied all tests in previous state

--- handlers/homePage_test.go
+++ handlers/homePage_test.go
@@ -1,80 +1,58 @@
 package handlers
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/Hanssen97/cloud_assignment1/constants"
 )
 
-// var inst aetest.Instance
-//
-// func TestMain(m *testing.M) {
-// 	//inst, _ = aetest.NewInstance(nil)
-//
-// 	//m.Run()
-//
-// 	//defer tearDown()s
-// }
+func TestHomePage(t *testing.T) {
+	req, err := inst.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal("Can't create request")
+	}
 
-func TestMain(t *testing.T) {
-	//inst, _ = aetest.NewInstance(nil)
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(HomePage)
 
-	//m.Run()
+	handler.ServeHTTP(recorder, req)
 
-	//defer tearDown()
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := constants.HOMEPAGE
+
+	if recorder.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			recorder.Body.String(), expected)
+	}
 }
 
-// func tearDown() {
-// 	if inst != nil {
-// 		inst.Close()
-// 	}
-// }
+func TestHomePageBadRequest(t *testing.T) {
+	req, err := inst.NewRequest("GET", "/erybad", nil)
+	if err != nil {
+		t.Fatal("Can't create request")
+	}
 
-// func TestHomePage(t *testing.T) {
-// 	var inst aetest.Instance
-// 	inst, _ = aetest.NewInstance(nil)
-//
-// 	req, err := inst.NewRequest("GET", "/", nil)
-// 	if err != nil {
-// 		t.Fatal("Can't create request")
-// 	}
-//
-// 	recorder := httptest.NewRecorder()
-// 	handler := http.HandlerFunc(HomePage)
-//
-// 	handler.ServeHTTP(recorder, req)
-//
-// 	if status := recorder.Code; status != http.StatusOK {
-// 		t.Errorf("handler returned wrong status code: got %v want %v",
-// 			status, http.StatusOK)
-// 	}
-//
-// 	expected := constants.HOMEPAGE
-//
-// 	if recorder.Body.String() != expected {
-// 		t.Errorf("handler returned unexpected body: got %v want %v",
-// 			recorder.Body.String(), expected)
-// 	}
-// }
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(HomePage)
 
-// func TestHomePageBadRequest(t *testing.T) {
-// 	req, err := inst.NewRequest("GET", "/erybad", nil)
-// 	if err != nil {
-// 		t.Fatal("Can't create request")
-// 	}
-//
-// 	recorder := httptest.NewRecorder()
-// 	handler := http.HandlerFunc(HomePage)
-//
-// 	handler.ServeHTTP(recorder, req)
-//
-// 	if status := recorder.Code; status != http.StatusNotFound {
-// 		t.Errorf("handler returned wrong status code: got %v want %v",
-// 			status, http.StatusNotFound)
-// 	}
-//
-// 	expected := `404 page not found`
-//
-// 	if strings.TrimSpace(recorder.Body.String()) != expected {
-// 		t.Errorf("handler returned unexpected body: got %v want %v",
-// 			recorder.Body.String(), expected)
-// 	}
-// }
+	handler.ServeHTTP(recorder, req)
+
+	if status := recorder.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusNotFound)
+	}
+
+	expected := `404 page not found`
+
+	if strings.TrimSpace(recorder.Body.String()) != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			recorder.Body.String(), expected)
+	}
+}
--- handlers/repo_test.go
+++ handlers/repo_test.go
@@ -1,110 +1,110 @@
 package handlers
 
-// import (
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"testing"
-//
-// 	"github.com/Hanssen97/cloud_assignment1/constants"
-// 	"github.com/Hanssen97/cloud_assignment1/testdata"
-// 	"github.com/jarcoal/httpmock"
-// 	"google.golang.org/appengine/aetest"
-// )
-//
-// var inst aetest.Instance
-//
-// func TestMain(m *testing.M) {
-// 	inst, _ = aetest.NewInstance(nil)
-//
-// 	m.Run()
-//
-// 	defer tearDown()
-// }
-// func tearDown() {
-// 	if inst != nil {
-// 		inst.Close()
-// 	}
-// }
-//
-// // Repo() ----------------------------------------------------
-// func TestRepo(t *testing.T) {
-// 	httpmock.Activate()
-// 	defer httpmock.DeactivateAndReset()
-//
-// 	httpmock.RegisterResponder("GET", constants.GITHUB+"repos/klyve/gritapp",
-// 		httpmock.NewStringResponder(200, testdata.REPOJSON))
-//
-// 	httpmock.RegisterResponder("GET", constants.GITHUB+"repos/klyve/gritapp/contributors",
-// 		httpmock.NewStringResponder(200, testdata.REPOCONTRIBUTORS))
-//
-// 	httpmock.RegisterResponder("GET", constants.GITHUB+"repos/klyve/gritapp/languages",
-// 		httpmock.NewStringResponder(200, testdata.REPOLANGUAGES))
-//
-// 	req, err := inst.NewRequest("GET", "/projectinfo/v1/github.com/klyve/gritapp", nil)
-// 	if err != nil {
-// 		t.Fatal("Can't create request")
-// 	}
-//
-// 	recorder := httptest.NewRecorder()
-//
-// 	handler := http.HandlerFunc(Repo)
-// 	handler.ServeHTTP(recorder, req)
-//
-// 	expected := testdata.REPORES
-//
-// 	if recorder.Body.String() != expected {
-// 		t.Errorf("handler returned unexpected body: got %v want %v",
-// 			recorder.Body.String(), expected)
-// 	}
-// }
-//
-// // getRepoMapShortUrl() -----------------------------------------------
-// func TestGetRepoMapShortURL(t *testing.T) {
-// 	req, err := inst.NewRequest("GET", "/URLThatIsNotLongEnough", nil)
-// 	if err != nil {
-// 		t.Fatal("Can't create request")
-// 	}
-//
-// 	recorder := httptest.NewRecorder()
-//
-// 	data, err := getRepoMap(recorder, req)
-//
-// 	expected := "Bad Request"
-//
-// 	if err.Error() != expected {
-// 		t.Errorf("Function returned unexpected error: got %v want %v",
-// 			data, expected)
-// 	}
-// }
-//
-// // Respond() --------------------------------------------------
-// func TestRespond(t *testing.T) {
-// 	data := Repository{
-// 		Project:   "github.com/test/test",
-// 		Owner:     "Tester",
-// 		Committer: "Committer",
-// 		Commits:   110,
-// 		Language:  []string{"JavaScript", "Go", "C++"},
-// 	}
-//
-// 	recorder := httptest.NewRecorder()
-//
-// 	respond(recorder, data)
-//
-// 	expected := `{
-//  "project": "github.com/test/test",
-//  "owner": "Tester",
-//  "committer": "Committer",
-//  "commits": 110,
-//  "language": [
-//   "JavaScript",
-//   "Go",
-//   "C++"
-//  ]
-// }`
-//
-// 	if recorder.Body.String() != expected {
-// 		t.Errorf("handler returned unexpected body: got %v want %v",
-// 			recorder.Body.String(), expected)
-// 	}
-// }
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Hanssen97/cloud_assignment1/constants"
+	"github.com/Hanssen97/cloud_assignment1/testdata"
+	"github.com/jarcoal/httpmock"
+	"google.golang.org/appengine/aetest"
+)
+
+var inst aetest.Instance
+
+func TestMain(m *testing.M) {
+	inst, _ = aetest.NewInstance(nil)
+
+	m.Run()
+
+	defer tearDown()
+}
+func tearDown() {
+	if inst != nil {
+		inst.Close()
+	}
+}
+
+// Repo() ----------------------------------------------------
+func TestRepo(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("GET", constants.GITHUB+"repos/klyve/gritapp",
+		httpmock.NewStringResponder(200, testdata.REPOJSON))
+
+	httpmock.RegisterResponder("GET", constants.GITHUB+"repos/klyve/gritapp/contributors",
+		httpmock.NewStringResponder(200, testdata.REPOCONTRIBUTORS))
+
+	httpmock.RegisterResponder("GET", constants.GITHUB+"repos/klyve/gritapp/languages",
+		httpmock.NewStringResponder(200, testdata.REPOLANGUAGES))
+
+	req, err := inst.NewRequest("GET", "/projectinfo/v1/github.com/klyve/gritapp", nil)
+	if err != nil {
+		t.Fatal("Can't create request")
+	}
+
+	recorder := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(Repo)
+	handler.ServeHTTP(recorder, req)
+
+	expected := testdata.REPORES
+
+	if recorder.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			recorder.Body.String(), expected)
+	}
+}
+
+// getRepoMapShortUrl() -----------------------------------------------
+func TestGetRepoMapShortURL(t *testing.T) {
+	req, err := inst.NewRequest("GET", "/URLThatIsNotLongEnough", nil)
+	if err != nil {
+		t.Fatal("Can't create request")
+	}
+
+	recorder := httptest.NewRecorder()
+
+	data, err := getRepoMap(recorder, req)
+
+	expected := "Bad Request"
+
+	if err.Error() != expected {
+		t.Errorf("Function returned unexpected error: got %v want %v",
+			data, expected)
+	}
+}
+
+// Respond() --------------------------------------------------
+func TestRespond(t *testing.T) {
+	data := Repository{
+		Project:   "github.com/test/test",
+		Owner:     "Tester",
+		Committer: "Committer",
+		Commits:   110,
+		Language:  []string{"JavaScript", "Go", "C++"},
+	}
+
+	recorder := httptest.NewRecorder()
+
+	respond(recorder, data)
+
+	expected := `{
+ "project": "github.com/test/test",
+ "owner": "Tester",
+ "committer": "Committer",
+ "commits": 110,
+ "language": [
+  "JavaScript",
+  "Go",
+  "C++"
+ ]
+}`
+
+	if recorder.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			recorder.Body.String(), expected)
+	}
+}
